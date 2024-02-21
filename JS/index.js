b4e598bd3d8a510bcea4bd8ee5a11a91e45eb0b0
kaziarman23 <kaziarman@proton.me>
2024-02-21 17:41:49 +0600

    project-accomplished

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,30 @@
 const boxs = document.querySelectorAll(".box")
-// const msg = document.querySelector("#msg")
-// const message = document.querySelector(".message_contener")
-const new_game_btn = document.querySelector(".new_game_btn")
-const reset_btn = document.querySelector(".reset_btn")
+const msg = document.querySelector("#msg")
+const megContener = document.querySelector(".message_contener")
+const newGameBtn = document.querySelector(".new_game_btn")
+const resetBtn = document.querySelector(".reset_btn")
 
 let turnO = true;
 
  const winPatten = [
     [0,1,2],
     [0,3,6],
-    [0,4,7],
+    [0,4,8],
     [1,4,7],
     [2,5,8],
     [2,4,6],
     [3,4,5],
     [6,7,8]
  ];
+
+ resetBtn.addEventListener("click", resetGame);
+ newGameBtn.addEventListener("click", resetGame);
+
+ const resetGame = () => {
+    turnO = true
+    enableBoxs()
+    megContener.classList.add("hide")
+}
 
  boxs.forEach( (box) => {
     box.addEventListener("click",function(){
@@ -41,14 +50,32 @@ let turnO = true;
         let pos3Val = boxs[pattern[2]].innerText;
 
         if (pos1Val != "" && pos2Val != "" && pos3Val != "" ){
-            if ( pos1Val === pos2Val === pos3Val) {
-                // showwinner(pos1Val);
+            if ( pos1Val === pos2Val && pos2Val=== pos3Val) {
+                 showwinner(pos1Val);
             }
         }
     }
  }
 
-//  const showwinner = (winner) => {
-//     msg.innerHTML = `And the winner is ${winner}`
-//     message.classList.remove("hide");
-//  }
+  const showwinner = (winner) => {
+     msg.innerHTML = `And the winner is ${winner}`
+     megContener.classList.remove("hide");
+     disabledBoxs();
+  }
+
+
+  function disabledBoxs(){
+    for(let box of boxs){
+        box.disabled = true;
+    }
+  }
+
+
+ function enableBoxs(){
+    for(let box of boxs){
+        box.disabled = false;
+        box.textContent = ""
+    }
+  }
+
+  
